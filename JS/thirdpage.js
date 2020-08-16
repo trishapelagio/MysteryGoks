fd01021f6aff0150834a8d4e57a192b0e7d98862
Trisha <48178740+trishapelagio@users.noreply.github.com>
2020-08-16 17:53:36 +0800

    fixed styles

---
+++ JS/thirdpage.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    console.log("hehe");
+
+    $("#code").keyup(function(){
+        var val = $("#code").val();
+        if(val == "1234"){
+            window.location.replace("page4.html");
+        }
+    })
+});
