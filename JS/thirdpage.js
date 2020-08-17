6c65dde40d0f28b2e1a6ee2f347d2a091a6be676
Lance Ricco Teng <47263399+S0rbetes@users.noreply.github.com>
2020-08-17 22:21:41 +0800

    Added 3rd section functionality

--- JS/thirdpage.js
+++ JS/thirdpage.js
@@ -1,10 +1,25 @@
-$(document).ready(function() {
-    console.log("hehe");
+function triggerDialogue() {
+    $(".msg").each(function(i, obj) {
+        setTimeout(() => {
+            $(this).removeClass("d-none");
+        }, 2500 * i);
+    });
+    $("#code").prop("disabled", false);
 
+}
+
+$(document).ready(async function() {
+
+    $("#callMessage").click(function() {
+        $("#chatLog").removeClass("d-none");
+        $("#badge").remove();
+        triggerDialogue();
+    });
+    
     $("#code").keyup(function(){
         var val = $("#code").val();
         if(val == "1234"){
             window.location.replace("page4.html");
         }
-    })
+    });
 });
